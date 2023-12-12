bcf79758802aaf49077ffd2e23e100bbe4fc350a
Nikita Nedvetskii <nnedvetskii@wallarm.com>
2023-12-12 11:31:34 +0100

    DEVOPS-2235 Switch container to AIO

---
+++ build-scripts/apply_fixes.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+ARCH=${ARCH:-x86_64}
+DOCKER_ARCH=${DOCKER_ARCH:-amd64}
+
+if [ "$ARCH" == "aarch64" ]; then
+    DOCKER_ARCH="arm64"
+fi
+
+BUILD_DIR="build/linux/${DOCKER_ARCH}"
+
+sed -i -E \
+    -e '/WALLARM_COMPONENT_NAME/s/(.*)=(.*)/\1=wallarm-nginx-docker/' \
+    -e "/WALLARM_COMPONENT_VERSION/s/(.*)=(.*)/\1=$AIO_VERSION/" \
+    -e '/SLAB_ALLOC_ARENA/d' \
+    "$BUILD_DIR/opt/wallarm/env.list"
+
+sed -i \
+    -e '/FQDNLookup/s/no/true/' \
+    -e '/DeleteSocket/aSocketGroup "wallarm"' \
+    "$BUILD_DIR/opt/wallarm/etc/collectd/wallarm-collectd.conf"
+
+sed -i -E \
+    -e '/LUA/s/(.*)=(.*)/\1="\2"/' \
+    "$BUILD_DIR/opt/wallarm/env.list"
+
+cat conf/supervisord.conf.socat >> "$BUILD_DIR/opt/wallarm/etc/supervisord.conf.filtering"
+
+mkdir -p "$BUILD_DIR/usr/lib/nginx/modules" && \
+    mv "$BUILD_DIR/opt/wallarm/modules/${WLRM_FOLDER}/"* "$BUILD_DIR/usr/lib/nginx/modules/" && \
+    rm -rf "$BUILD_DIR/opt/wallarm/modules"
---
+++ build-scripts/get_dependencies.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+ARCH=${ARCH:-x86_64}
+DOCKER_ARCH=${DOCKER_ARCH:-amd64}
+
+if [ "$ARCH" == "aarch64" ]; then
+    DOCKER_ARCH="arm64"
+fi
+
+AIO_FILE="wallarm-${AIO_VERSION}.${ARCH}-musl.tar.gz"
+AIO_URL="https://storage.googleapis.com/meganode_storage/${AIO_VERSION%.*}/${AIO_FILE}"
+BUILD_DIR="build/linux/${DOCKER_ARCH}"
+
+echo "Downloading AiO archive ${AIO_FILE} ..."
+curl --create-dirs -L -C - -o "$BUILD_DIR/wallarm-${ARCH}.tar.gz" "$AIO_URL"
+
+echo "Extracting into ${BUILD_DIR}"
+tar -xf "$BUILD_DIR/wallarm-${ARCH}.tar.gz" -C "$BUILD_DIR"
